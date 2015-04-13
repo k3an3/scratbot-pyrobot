84e5a7a503cac2ae69f4c6fc1df9210300eb6f43
Keane O'Kelley <kokelley@iastate.edu>
2015-04-12 19:59:07 -0500

    Fix Status subclass initializers

--- status.py
+++ status.py
@@ -36,21 +36,25 @@ class MoveForwardStatus(Status):
     def __init__(self, distance, abort_reason):
         self.distance_actually_moved = distance
         self.abort_reason = abort_reason
+        Status.__init__(self)
 
 class MoveReverseStatus(Status):
 
     def __init__(self, distance, abort_reason):
         self.distance_actually_moved = distance
         self.abort_reason = abort_reason
+        Status.__init__(self)
 
 class RotateClockwiseStatus(Status):
 
     def __init__(self, degrees, abort_reason):
         self.degrees_actually_turned = degrees
         self.abort_reason = abort_reason
+        Status.__init__(self)
 
 class RotateCounterclockwiseStatus(Status):
 
     def __init__(self, degrees, abort_reason):
         self.degrees_actually_turned = degrees
         self.abort_reason = abort_reason
+        Status.__init__(self)
