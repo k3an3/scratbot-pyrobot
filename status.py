e12c6bd300729a1a23a7d99280ec5cf675ed3bba
Keane O'Kelley <kokelley@iastate.edu>
2015-04-12 19:01:53 -0500

    Add status classes

--- status.py
+++ status.py
@@ -1,1 +1,32 @@
+from sender import *
+
+class Status:
+
+    def __init__(self, command_id):
+        self.command = getCommandByID(id)
+
+class MoveForwardStatus(Status):
+
+    def __init__(self, distance, abort_reason):
+        self.distance_actually_moved = distance
+        self.abort_reason = abort_reason
+
+class MoveForwardStatus(Status):
+
+    def __init__(self, distance, abort_reason):
+        self.distance_actually_moved = distance
+        self.abort_reason = abort_reason
+
+class RotateClockwiseStatus(Status):
+
+    def __init__(self, degrees, abort_reason):
+        self.degrees_actually_turned = degrees
+        self.abort_reason = abort_reason
+
+class RotateCounterclockwiseStatus(Status):
+
+    def __init__(self, degrees, abort_reason):
+        self.degrees_actually_turned = degrees
+        self.abort_reason = abort_reason
+
 
